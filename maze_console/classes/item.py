d7e5c91756dd0b5144e4ab38107d323271e8c5bc
Mickael Perchet <mickael.perchet@gmail.com>
2019-10-04 14:29:32 +0200

    Maze console OK. Commit avant Pygame

--- maze_console/classes/item.py
+++ maze_console/classes/item.py
@@ -5,25 +5,14 @@ class Item:
     """Items to pick"""
     # Ajouter sous-classes pour chaque item pour
     # personnaliser les blit dans pygame
-    item_list = list()
+    list_items = []
 
     def __init__(self, carte):
         """
         Position de départ aléatoire dans la liste des cases autorisées
         """
-        if Item.item_list == []:
-            self.position = carte.allowed_tiles[
-                random.randint(0, len(carte.allowed_tiles) - 1)
-                ]
-            Item.item_list.append(self.position)
-        else:
-            test = True
-            while test:
-                position_test = carte.allowed_tiles[
-                    random.randint(0, len(carte.allowed_tiles) - 1)
-                ]
-                if position_test not in Item.item_list:
-                    self.position = position_test
-                    test = False
+        if Item.list_items == []:
+            Item.list_items = random.sample(carte.allowed_tiles, 3)
+        self.position = Item.list_items.pop(0)
         self.posx = self.position[0]
         self.posy = self.position[1]
